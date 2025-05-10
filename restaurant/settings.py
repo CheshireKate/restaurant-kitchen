c0ac6101732b0f949b862d824da57c1ecea4a40d
Ekaterina <sudarkova.ekaterina@gmail.com>
2025-05-11 01:26:14 +0300

    added secret key to settings uding getenv

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-370999df!8cg64l!=cm&si04w8!lsji+c2q*-3k7t)tto2lfvc'
+SECRET_KEY = os.getenv("DB_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

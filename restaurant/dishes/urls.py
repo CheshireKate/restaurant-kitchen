b2d219d7c96f8f3a4fafdaba0bf87698753a0f0f
Ekaterina <sudarkova.ekaterina@gmail.com>
2025-06-04 00:56:21 +0300

    corrected

--- restaurant/dishes/urls.py
+++ restaurant/dishes/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 from .views import DishListView, DishDetailView, DishUpdateView, DishDeleteView
 
 urlpatterns = [
-    path("restaurant/dishes/", DishListView, namespace="dishes=list"),
+    path("restaurant/dishes/", DishListView.as_view(), name="dishes=list"),
     path("restaurant/dishes/<int:pk>/", DishDetailView.as_view(), name="cook-detail"),
     path("restaurant/dishes/<int:pk>/update", DishUpdateView.as_view(), name="cook-update"),
     path("restaurant/dishes/<int:pk>/delete", DishDeleteView.as_view(), name="cook-delete"),
